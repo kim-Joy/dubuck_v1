fb34e898ff573422ec00834a87d69a2cd8248ed3
kim joy <didlshakdi@gmail.com>
2024-04-16 02:22:03 +0900

    절대경로 추가

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,23 @@
+//react npm 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+// Page route
+import Intro from "./page/Intro";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route path="/"  element={<Intro />}></Route>
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ app/src/page/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Intro() {
+
+  return(
+     <div className="intro">
+        sssss
+     </div>
+  )
+}
+
+
+export default Intro;
